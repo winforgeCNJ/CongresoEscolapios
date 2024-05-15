a9ed9835fb0da89c0604c32d76fb8439442c19c0
Naim Chaya <naimchaya.dev@gmail.com>
2024-05-15 20:05:25 -0300

    Workflow added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  images: {
+    domains: ["images.pexels.com", "st3.depositphotos.com"],
+  },
+};
 
 export default nextConfig;
