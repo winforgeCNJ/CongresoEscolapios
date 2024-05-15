cd437ca9f97ff262efbef02f42ee776a940364e4
Naim Chaya <naimchaya.dev@gmail.com>
2024-05-15 20:26:44 -0300

    nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  images: {
-    domains: ["images.pexels.com", "st3.depositphotos.com"],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
