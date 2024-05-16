0448e215b0d3f8e5c62f6485182b44deb034666c
Naim Chaya <naimchaya.dev@gmail.com>
2024-05-15 21:24:46 -0300

    Domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/CongresoEscolapios" : "",
   images: {
     domains: ["images.pexels.com", "st3.depositphotos.com"],
   },
