a961f2ce854d2e89ed6e002a9658e922d9340d22
winforgeCNJ <168235835+winforgeCNJ@users.noreply.github.com>
2024-05-15 21:15:46 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,6 @@
 const nextConfig = {
   output: "export",
   basePath: process.env.NODE_ENV === "production" ? "/CongresoEscolapios" : "",
-  assetPrefix:
-    process.env.NODE_ENV === "production" ? "/CongresoEscolapios/" : "",
   images: {
     domains: ["images.pexels.com", "st3.depositphotos.com"],
   },
