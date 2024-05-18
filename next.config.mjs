d1ca02f564c7107b7324ddc1300276103f9a996f
Naim Chaya <naimchaya.dev@gmail.com>
2024-05-17 23:34:35 -0300

    next.config.msj to work in Local

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   images: {
     domains: ["images.pexels.com", "st3.depositphotos.com"],
   },
 };
+
+if (process.env.NODE_ENV === "production") {
+  nextConfig.output = "export";
+}
 
 export default nextConfig;
