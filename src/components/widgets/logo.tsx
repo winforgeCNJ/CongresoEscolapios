621b89926e7d310197c05e093f9b7eb3fd691303
Naim Chaya <naimchaya.dev@gmail.com>
2024-05-17 23:33:28 -0300

    Header updated

--- src/components/widgets/logo.tsx
+++ src/components/widgets/logo.tsx
@@ -5,9 +5,10 @@ export default function Logo() {
     <img
       src="/logo.png"
       alt="logo"
-      height="256"
-      width="128"
-      className="h-24 w-44 lg:h-[6rem] lg:w-[12rem]"
+      // height="256"
+      // width="128"
+      // className="h-24 w-44 lg:h-[6rem] lg:w-[12rem]"
+      className="w-32 pb-2 lg:w-[9rem] "
     />
   );
 }
--- src/components/widgets/menu.tsx
+++ src/components/widgets/menu.tsx
@@ -10,7 +10,7 @@ export default function Menu() {
           <li key={nav.id} className=" ">
             <Link
               href={nav.to}
-              className="py-6  font-medium transition-colors hover:text-secondary lg:text-sm  "
+              className="py-6  font-medium transition-colors hover:text-secondary lg:text-base  "
             >
               {nav.title}
             </Link>
