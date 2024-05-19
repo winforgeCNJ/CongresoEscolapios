4ab8f0152e4f0750d8adcfc51282934119499830
matias-d <matiasc.02.2003@gmail.com>
2024-05-19 15:43:50 -0300

    FIXED : touch normal

--- src/components/sections/objectives/objectives-carousel.tsx
+++ src/components/sections/objectives/objectives-carousel.tsx
@@ -30,7 +30,7 @@ export default function ObjectivesCarousel() {
   return (
     <section
       className="flex h-screen w-full lg:w-auto flex-col items-center justify-center gap-y-4 bg-primary relative "
-      style={{ touchAction: 'none' }}
+     
     >
       <div className="bg-primary w-full h-full lg:w-[40vw] ">
         {objectives.map((objective) => (
