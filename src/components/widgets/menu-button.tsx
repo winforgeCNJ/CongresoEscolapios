7488c8c07881846b0e026670969ff60742123e58
matias-d <matiasc.02.2003@gmail.com>
2024-05-20 18:16:11 -0300

    FEAT: use click outised hook

--- src/components/widgets/menu-button.tsx
+++ src/components/widgets/menu-button.tsx
@@ -1,13 +1,18 @@
 import { IconMenu } from "../icons/icon-menu";
 import IconClose from "../icons/icon-close";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { Socials } from "./socials";
 import Menu from "./menu";
+import useClickOutisde from "@/hooks/useClickOutisde";
 
 export default function MenuButton() {
   const [menu, setMenu] = useState(false);
 
   const onMenu = () => setMenu(!menu);
+
+  const asideRef = useRef<HTMLDivElement>(null)
+
+  useClickOutisde({ ref : asideRef, isOpen : menu, onClose : onMenu  })
 
   return (
     <>
@@ -16,6 +21,7 @@ export default function MenuButton() {
       </button>
 
       <aside
+        ref={asideRef}
         className={`${menu ? "right-0" : "-right-full"} fixed top-0 z- flex h-screen   flex-col items-center justify-center gap-y-4 w-64 bg-primary text-white transition-all duration-200 ease-in-out lg:hidden`}
       >
         <Menu />
