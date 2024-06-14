1b576df655e6e6d2cc95262d982c9ecd9f9585fc
Naim Chaya <naimchaya.dev@gmail.com>
2024-06-14 18:38:08 -0300

    IconUbication

--- src/consts/socials.ts
+++ src/consts/socials.ts
@@ -1,4 +1,5 @@
 import IconInstagram from "@/components/icons/icon-instagram";
+import IconUbicacion from "@/components/icons/icon-ubicacion";
 import IconWhatsapp from "@/components/icons/icon-whatsapp";
 
 export const socials = [
@@ -11,5 +12,10 @@ export const socials = [
     id: 2,
     icon: IconWhatsapp,
     to: "https://api.whatsapp.com/send?phone=5493584393164",
+  },
+  {
+    id: 3,
+    icon: IconUbicacion,
+    to: "https://maps.app.goo.gl/N5Ry8s58TtF2RPNY7?g_st=iw",
   },
 ];
