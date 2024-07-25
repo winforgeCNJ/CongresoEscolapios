d205ca8d8d59aa9a9fadd51f8e0f3a145d0d69de
matias-d <matiasc.02.2003@gmail.com>
2024-07-25 18:46:21 -0300

    chore: :package: install tailwind animate

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
-import type { Config } from "tailwindcss";
 
+import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -19,6 +19,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animated')],
 };
 export default config;
