8756a2292131948461eb61c019f2152f351f91e1
Naim Chaya <naimchaya.dev@gmail.com>
2024-08-08 19:17:02 -0300

    duedate

--- src/components/widgets/popup.tsx
+++ src/components/widgets/popup.tsx
@@ -36,8 +36,8 @@ export default function Popup() {
               <img src='/assets/themes/theme-3.webp' width={190} height={384} className='lg:w-[11.875rem] lg:h-[24rem] h-44 w-full object-cover object-top block lg:hidden'/>
             </div>
             <section className='lg:pl-6 pl-4 pr-4 text-primary pt-8 lg:pt-16'>
-              <h3 className='text-base lg:text-xl font-semibold -mb-1'>¡No te pierdas esta oportunidad!</h3>
               <h3 className={`text-xl lg:text-3xl font-medium lg:mb-2 ${alegreya.className}`}>Inscribite al Congreso Humanista</h3>
+              <h3 className='text-base lg:text-xl font-semibold -mb-1'>¡Tenes tiempo hasta el 15 de Agosto!</h3>  
               <p className='text-primary/80 text-sm mb-4'>Se acerca la fecha y te esperamos para compartir en el Colegio Santo Tomás, Córdoba, Argentina, los días 30, 31 de agosto y 01 de septiembre.</p>
               <button onClick={handleNavigate}  className="group relative inline-flex  items-center justify-center overflow-hidden text-primary to-secondary shadow-secondary/15  rounded-full bg-gradient-to-r from-white via-white px-4 py-2 text-sm tracking-wide shadow-lg transition-all hover:scale-[1.04] "><div className="mr-0 w-0 -translate-x-[100%] opacity-0 transition-all duration-200 group-hover:mr-1 group-hover:w-5 group-hover:translate-x-0 group-hover:opacity-100"><IconArrowRigth2 /></div><span>Inscribite ahora</span></button>
             </section>
